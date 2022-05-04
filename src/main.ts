fe16e6e9c970e671e061a2bbd5576101313b7aa3
Yauheni Yeremtsko <evgen.eremetko@gmail.com>
2022-05-04 16:58:23 +0300

    Added protection from unauthorized users

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { Quasar } from "quasar";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import quasarUserOptions from "./quasar-user-options";
+
+router.beforeEach((to) => {
+  const isAuth = localStorage.getItem("isAuth");
+  if (isAuth) {
+    store.commit("setAuth", isAuth);
+  }
+  if (!store.getters["getAuth"] && to.name !== "auth") return { name: "auth" };
+});
 
 createApp(App)
   .use(Quasar, quasarUserOptions)
